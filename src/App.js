be081c4a6ac34cc0fe9374104a6708afb9eca8d4
Rashidul Hasan <rashidul69@gmail.com>
2019-09-03 17:18:28 +0600

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/bootstrap.min.css';
+import "./App.css"
+import uuid from "uuid";
+import {InputBox} from "./components/InputBox";
+import {ProgressBar} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+      todos: [],
+      progress: 0
+  }
+
+    onTodoStateChange = (e) => {
+      const id = e.target.value;
+      const isChecked = e.target.checked;
+      const {todos} = this.state;
+
+      todos.map(item => {
+          if (item.id == id) {
+              item.done = isChecked;
+          }
+          return item;
+      });
+
+      // const progress = this.calculatePorgress(todos);
+      this.setState({todos})
+      console.log(e.target.value);
+      console.log(e.target.checked);
+
+    }
+
+    addTodo = (todo) => {
+      const {todos} = this.state;
+
+      if (todo === "") {
+        return;
+      }
+
+      todos.push({id: uuid(), text: todo, done: false});
+      this.setState({todos});
+    }
+
+  render() {
+
+
+      const {todos} = this.state;
+
+      const progress = this.calculatePorgress(todos);
+
+      console.log({progress});
+      return (
+          <div className="App">
+              <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                  <h1 className="display-4">React TODO <span role="img" aria-label="sheep">🤞</span></h1>
+              </div>
+
+              <div className="container">
+                  
+                  <div className="d-flex justify-content-center flex-row">
+                      <div className="todo-wrap">
+
+                          <InputBox onAddTodo={this.addTodo} />
+                          
+                          {/*<form className="form-inline" onSubmit={this.addTodo}>
+                            <input type="text" className="form-control mb-2 mr-sm-2"
+                                   placeholder="Add a todo..." id="input-todo" />
+                            <button type="submit" className="btn btn-primary mb-2">Add</button>
+                          </form>*/}
+
+                          { (progress > 0) &&
+                            <ProgressBar now={progress} label={`${progress}% Completed`} className="progress-container" />
+                          }
+
+                          <ul className="list-group">
+                              {this.state.todos.map (i  => {
+                                return (
+                                    <div className="list-group-item" key={i.id}>
+                                        <div className="form-check">
+                                            <input className="form-check-input"
+                                                   type="checkbox" defaultChecked={i.done}
+                                                   onClick={this.onTodoStateChange} value={i.id}/>
+                                                <label className={i.done ? "form-check-label strike-through" : "form-check-label"} htmlFor="gridCheck1">
+                                                    {i.text}
+                                                </label>
+                                        </div>
+
+                                    </div>
+
+                                )
+                              })}
+                          </ul>
+                      </div>
+                  </div>
+
+              </div>
+              <div className="container">
+                  <footer className="pt-4 my-md-5 pt-md-5 border-top">
+                      <div className="row">
+
+                      </div>
+                  </footer>
+              </div>
+          </div>
+      );
+  }
+
+    calculatePorgress = (todos) => {
+      const total = todos.length;
+
+      if (total === 0) return 0;
+      let done = 0;
+      todos.map( i => {
+          if (i.done) done++;
+      });
+
+      return Math.ceil((done/total) * 100)
+    }
 }
 
 export default App;
