29c3ac77a9916a1b0c47e635f794cde5196d85d3
Rashidul Hasan <rashidul69@gmail.com>
2019-09-04 18:54:04 +0600

    Updates

---
+++ precache-manifest.ff4194756e2f76cc15146a8a17503d5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7e9bbab81f5d574404f7113d24c77c",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "8a15c664478031a1bc0c",
+    "url": "/react-todo/static/css/main.2a701bc7.chunk.css"
+  },
+  {
+    "revision": "8807e4951b29155afcce",
+    "url": "/react-todo/static/js/2.22318de7.chunk.js"
+  },
+  {
+    "revision": "8a15c664478031a1bc0c",
+    "url": "/react-todo/static/js/main.db96d5e3.chunk.js"
+  },
+  {
+    "revision": "2f3a917ddc7c04fc2202",
+    "url": "/react-todo/static/js/runtime~main.0271e842.js"
+  }
+]);
